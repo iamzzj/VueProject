d7e06c0e7ff5eeb56d315455d2d03fe55a59c4b8
iamzzj <iamzzj@qq.com>
2021-01-31 19:49:35 +0800

    单页面应用demo1

---
+++ hello-ui/demo1.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, index) in indexs"
+          :key="index"
+          @click="choose(index)"
+          :class="{active: index == current && current !== ''}"
+      >
+        {{ item }}
+      </li>
+    </ul>
+    <button type="button" @click="add()">添加</button>
+    <ul>
+      <li v-for="(item, index) in target" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'demo1',
+  data () {
+    return {
+      current: '',
+      indexs: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      target: []
+    }
+  },
+  methods: {
+    choose (index) {
+      this.current = index
+    },
+    add () {
+      if (this.current === '') {
+        return
+      }
+      this.target.push(this.indexs[this.current])
+      this.current = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+li.active {
+  background: green;
+}
+</style>
