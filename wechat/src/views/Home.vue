16ee1f87f61058bb3794da1ce4a0b33805c5974a
iamzzj <iamzzj@qq.com>
2021-02-21 15:27:13 +0800

    global.scss

--- wechat/src/views/Home.vue
+++ wechat/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <router-view />
+    <router-view/>
     <ul class="footer">
       <li class="icons">
         <router-link :to="{name: 'WeChat'}">微信</router-link>
@@ -29,32 +29,37 @@
 </script>
 
 <style scoped lang="scss">
-li {
-  list-style: none;
-}
-.footer{
-  position: fixed;
-  width: 100%;
-  height: 60px;
-  line-height: 60px;
-  left: 0;
-  bottom: 0;
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-around;
-}
-.icons{
-  font-size: 16px;
-  flex: 1;
-  text-align: center;
-  border-top: 1px solid #42b983;
-  a{
-    color: #42b983;
-    display: block;
-    &.active{
-      color: #fff;
-      background: #42b983;
+  @import "src/css/global";
+
+  li {
+    list-style: none;
+  }
+
+  .footer {
+    position: fixed;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+  }
+
+  .icons {
+    font-size: 16px;
+    flex: 1;
+    text-align: center;
+    border-top: 1px solid $colorPrimary;
+
+    a {
+      color: $colorPrimary;
+      display: block;
+      &.active {
+        color: white;
+        background: $colorPrimary;
+      }
     }
   }
-}
 </style>
--- wechat/src/views/WeChat.vue
+++ wechat/src/views/WeChat.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <h1>WeChat</h1>
-    <img height="20px" width="20px" src="../assets/logo.png">
     <ul v-for="(chat, index) in chats" :key="index">
-      <li>{{chat.name}}</li>
+      <li v-bind:class="{WeChatItemDivision: index > 0}"><div class="WeChatItem">{{ chat.name }}</div></li>
     </ul>
   </div>
 </template>
@@ -32,9 +31,23 @@
   }
 </script>
 
-<style scoped>
-  img {
-    float: right;
-    margin-left: fill;
+<style scoped lang="scss">
+  @import "src/css/global";
+
+  li {
+    height: 40px;
+    width: 100%;
+    display: table;
+  }
+
+  .WeChatItem {
+    display: table-cell;
+    vertical-align: middle;
+    padding-left: $itemPadding;
+    padding-right: $itemPadding;
+  }
+
+  .WeChatItemDivision {
+    border-top: 1px solid $colorPrimary;
   }
 </style>
