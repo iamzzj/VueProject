00f1f1bd3f53129c489058daa8034b876c840503
iamzzj <iamzzj@qq.com>
2021-02-28 16:02:44 +0800

    vue_shop login

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI, { Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$message = Message
+
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1'
+Vue.prototype.$http = axios
 
 new Vue({
   router,
--- vue_shop/src/router/index.js
+++ vue_shop/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login'
+import Home from '../views/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
 ]
 
@@ -24,6 +26,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+/* 挂在路由导航守卫 */
+router.beforeEach((to, from, next) => {
+  // to将要访问的路径
+  // from代表从哪个路径跳转而来
+  // net 是一个函数，表示放行
+  if (to.path === '/login') return next()
+  // get token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
