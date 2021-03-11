213b972d20230d66860e8937bfad9a77ad8fac9f
iamzzj <iamzzj@qq.com>
2021-03-11 23:19:12 +0800

    vue_shop goods 80%

--- vue_shop/src/components/goods/Params.vue
+++ vue_shop/src/components/goods/Params.vue
@@ -42,7 +42,32 @@
             :data="manyTableData"
             border
             stripe>
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <template slot-scope="scope">
+                <el-tag
+                  v-for="(item, i) in scope.row.attr_vals"
+                  :key="i"
+                  closable
+                  @close="handleClose(i, scope.row)">
+                  {{ item }}
+                </el-tag>
+                <el-input
+                  class="input-new-tag"
+                  v-if="scope.row.inputVisible"
+                  v-model="scope.row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(scope.row)"
+                  @blur="handleInputConfirm(scope.row)"
+                >
+                </el-input>
+                <el-button
+                  v-else class="button-new-tag"
+                  size="small" @click="showInput(scope.row)">
+                  + New Tag
+                </el-button>
+              </template>
+            </el-table-column>
             <el-table-column label="#" type="index"></el-table-column>
             <el-table-column
               label="参数名称"
@@ -60,6 +85,7 @@
                 <el-button
                   size="mini"
                   type="danger"
+                  @click="removeParams(scope.row.attr_id)"
                   icon="el-icon-delete">
                   删除
                 </el-button>
@@ -81,7 +107,32 @@
             :data="onlyTableData"
             border
             stripe>
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <template slot-scope="scope">
+                <el-tag
+                  v-for="(item, i) in scope.row.attr_vals"
+                  :key="i"
+                  closable
+                  @close="handleClose(i, scope.row)">
+                  {{ item }}
+                </el-tag>
+                <el-input
+                  class="input-new-tag"
+                  v-if="scope.row.inputVisible"
+                  v-model="scope.row.inputValue"
+                  ref="saveTagInput"
+                  size="small"
+                  @keyup.enter.native="handleInputConfirm(scope.row)"
+                  @blur="handleInputConfirm(scope.row)"
+                >
+                </el-input>
+                <el-button
+                  v-else class="button-new-tag"
+                  size="small" @click="showInput(scope.row)">
+                  + New Tag
+                </el-button>
+              </template>
+            </el-table-column>
             <el-table-column label="#" type="index"></el-table-column>
             <el-table-column
               label="属性名称"
@@ -99,6 +150,7 @@
                 <el-button
                   size="mini"
                   type="danger"
+                  @click="removeParams(scope.row.attr_id)"
                   icon="el-icon-delete">
                   删除
                 </el-button>
@@ -266,11 +318,13 @@
       async getParamsData () {
         if (this.selectedCateKeys.length !== 3) {
           this.selectedCateKeys = []
+          this.manyTableData = []
+          this.onlyTableData = []
           return
         }
 
         // 选中的是三级分类
-        console.log(this.selectedCateKeys)
+        // console.log(this.selectedCateKeys)
 
         // 根据所选分类的id，和当前所处面板，获取对应的参数
         const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, {
@@ -281,6 +335,13 @@
           return this.$message.error('获取参数列表失败！')
         }
 
+        res.data.forEach(item => {
+          item.attr_vals = item.attr_vals ? item.attr_vals.split(' ') : []
+          // 控制文本框显示与隐藏
+          item.inputVisible = false
+          // 文本输入框的值
+          item.inputValue = ''
+        })
         if (this.activeName === 'many') {
           this.manyTableData = res.data
         } else {
@@ -352,6 +413,73 @@
       // 编辑对话框关闭
       editDialogClose () {
         this.$refs.editFormRef.resetFields()
+      },
+      // 根据id删除对应的参数项
+      async removeParams (attrId) {
+        const confirmResult = await this.$confirm(
+          '此操作将永久删除该参数，是否继续？',
+          '提示', {
+            confirmButtonText: '',
+            cancelButtonText: '',
+            type: 'warning'
+          }
+        ).catch(err => err)
+
+        if (confirmResult !== 'confirm') {
+          return this.$message.info(' 已取消删除！')
+        }
+
+        const { data: res } = await this.$http.delete(
+          `categories/${this.cateId}/attributes/${attrId}`)
+
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除参数失败！')
+        }
+        this.$message.success('删除成功！')
+        this.getParamsData()
+      },
+      // 文本框失去焦点，或按下enter键
+      async handleInputConfirm (row) {
+        if (row.inputValue.trim().length === 0) {
+          row.inputValue = ''
+          row.inputVisible = false
+          return
+        }
+        // 如果没有return，则输入的内容需要做后续处理
+        row.attr_vals.push(row.inputValue.trim())
+        row.inputValue = ''
+        row.inputVisible = false
+
+        this.saveAttrVals(row)
+      },
+      // 点击按钮展示文本输入框
+      showInput (row) {
+        row.inputVisible = true
+        // 让文本框自动获得焦点
+        // $nextTick当页面上元素被重新渲染后，才能指定回调函数中的代码
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus()
+        })
+      },
+      // 将对 attr_vals 的操作保存数据库
+      async saveAttrVals (row) {
+        const { data: res } = await this.$http.put(
+          `categories/${this.cateId}/attributes/${row.attr_id}`, {
+            attr_name: row.attr_name,
+            attr_sel: row.attr_sel,
+            attr_vals: row.attr_vals.join(' ')
+          })
+
+        if (res.meta.status !== 200) {
+          return this.$message.error('修改参数失败！')
+        }
+
+        this.$message.success('修改参数成功！')
+      },
+      // 删除对应的参数和选项
+      async handleClose (i, row) {
+        row.attr_vals.splice(i, 1)
+        this.saveAttrVals(row)
       }
     }
   }
@@ -359,6 +487,23 @@
 
 <style scoped lang="scss">
   .el-cascader {
-    margin-top: 15px;
+    margin-top: 15px
+  }
+
+  .el-tag {
+    margin: 10px
+  }
+
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .input-new-tag {
+    width: 120px;
+    margin-left: 10px;
   }
 </style>
